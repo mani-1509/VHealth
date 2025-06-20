8b2bfb32debba18c1d0838148a57d85b3977fab5
mani1509 <gajulamani10@gmail.com>
2025-06-20 15:37:01 +0530

    update!

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+from supabase import create_client
+from routes.auth import auth_bp
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route('/', methods=['GET'])
+app.register_blueprint(auth_bp)
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+@app.route('/')
 def base():
-    return render_template("base.html")
+    user = session.get('user')
+    username = session.get('username')  # Add this line
+    return render_template('base.html', user=user, username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
